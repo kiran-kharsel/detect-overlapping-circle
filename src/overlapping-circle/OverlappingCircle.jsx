436743d9787ba07363bbbedd707238e7fd25a861
kiran-kharsel <kirankharsel@gmail.com>
2026-03-20 07:08:44 +0530

    feat: create circle component

--- src/overlapping-circle/OverlappingCircle.jsx
+++ src/overlapping-circle/OverlappingCircle.jsx
@@ -3,8 +3,18 @@ import './style.css'
 
 function OverlappingCircle() {
   return (
-    <div>OverlappingCircle</div>
+    <div className='overlapping-circle'>
+        <Circle/>
+    </div>
   )
 }
 
 export default OverlappingCircle
+
+
+// circle component
+function Circle(){
+    return (
+        <div className='circle'></div>
+    )
+}
